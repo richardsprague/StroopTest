1940c0334d3e61e91f218cc0e96701152137b893
Richard Sprague <richardsprague@gmail.com>
2020-06-25 11:58:30 -0700

    Initial app

--- StroopHello/ContentView.swift
+++ StroopHello/ContentView.swift
@@ -12,27 +12,41 @@ struct ContentView: View {
     @State private var selection = 0
  
     var body: some View {
-        TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
+ 
+            HStack {
+        
+                TabView(selection: $selection){
+                    Text("Home")
+                        .font(.largeTitle)
+                        .tabItem {
+                            VStack {
+                                Image("home-7")
+                                Text("First")
+                            }
+                        }
+                        .tag(0)
+                    Text("Results")
+                        .font(.title)
+                        .tabItem {
+                            VStack {
+                                Image("Chart_Up")
+                                Text("Results")
+                            }
+                        }
+                        .tag(1)
+                    Text("Settings are here")
+                        .font(.title)
+                        .tabItem {
+                            VStack {
+                                Image("config30x30")
+                                Text("Settings")
+                            }
+                        }
+                        .tag(2)
                 }
-                .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
-                }
-                .tag(1)
+            }
         }
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
