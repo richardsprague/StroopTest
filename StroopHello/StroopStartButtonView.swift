f1eefdb33d38a952f431b6dd33eb7ff83bafabd5
Richard Sprague <richardsprague@gmail.com>
2020-06-30 17:11:33 -0700

    Refactoring everything into STCard class

--- StroopHello/StroopStartButtonView.swift
+++ StroopHello/StroopStartButtonView.swift
@@ -54,7 +54,7 @@ struct FancyButtonView: View {
             }
         }.sheet(isPresented: $wasPushed,
         onDismiss: { self.wasPushed = false }) {
-            STCardView(wasPushed:self.$wasPushed, discipline: STColor(message:"green"))
+            STCardView(wasPushed:self.$wasPushed, card: STCard(message:"green"))
          }
         
 //        .alert(isPresented: $wasPushed) {
