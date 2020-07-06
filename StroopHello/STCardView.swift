b92a9364bfb8ab19dd4e340af841a73d543f6891
Richard Sprague <richardsprague@gmail.com>
2020-07-06 11:34:25 -0700

    added new files

---
+++ StroopHello/STCardView.swift
@@ -0,0 +1,65 @@
+//
+//  STCardView.swift
+//  StroopHello
+//
+//  Created by Richard Sprague on 7/6/20.
+//  Copyright © 2020 Richard Sprague. All rights reserved.
+//
+
+import SwiftUI
+
+struct STCardView: View {
+    @Binding var wasPushed: Bool
+    @EnvironmentObject var STCardData: STCardData
+    @State private var counter = 0
+    
+    var card: STCard
+  var body: some View {
+    VStack {
+        Rectangle()
+            .fill(self.card.sColor)
+            .frame(width:300, height:400)
+            .padding()
+            .border(self.card.sColor, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+        
+        Text(card.color)
+        HStack {
+            Button(action: {
+ 
+                self.card.randomize()
+                self.card.score+=1
+                self.counter = self.card.score
+
+                
+            }){
+                Text(self.card.message)
+            }.padding()
+            ForEach(self.card.buttonsShuffled, id: \.self){ color in
+            Button(action: {self.wasPushed = false}){
+                Text(color)
+            }.padding()
+            }
+     
+        }
+        
+        
+
+        Button(action: {self.wasPushed = false}) {
+        Text("Done")
+        }
+        Text("State: " + String(self.counter))
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Image(systemName: "backward.fill")
+                .resizable()
+                .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+            
+        }
+}}}
+
+
+struct STCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        STCardView(wasPushed: .constant(false), card: STCard(message:"preview"))
+        .environmentObject(STCardData())
+    }
+}
--- StroopHello/STCardViewModel.swift
+++ StroopHello/STCardViewModel.swift
@@ -9,65 +9,11 @@
 import SwiftUI
 
 
-struct STCardViewModel: View {
-    @Binding var wasPushed: Bool
-    @EnvironmentObject var STCardData: STCardData
-    @State private var counter = 0
+class STCardViewModel:  ObservableObject {
     
-    var card: STCard
-  var body: some View {
-    VStack {
-        Rectangle()
-            .fill(self.card.sColor)
-            .frame(width:300, height:400)
-            .padding()
-            .border(self.card.sColor, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-        
-        Text(card.color)
-        HStack {
-            Button(action: {
- 
-                self.card.randomize()
-                self.card.score+=1
-                self.counter = self.card.score
-
-                
-            }){
-                Text(self.card.message)
-            }.padding()
-            ForEach(self.card.buttonsShuffled, id: \.self){ color in
-            Button(action: {self.wasPushed = false}){
-                Text(color)
-            }.padding()
-            }
-     
-        }
-        
-        
-
-        Button(action: {self.wasPushed = false}) {
-        Text("Done")
-        }
-        Text("State: " + String(self.counter))
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-            Image(systemName: "backward.fill")
-                .resizable()
-                .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
-            
-        }
-
-        
-
-    }
+   
     
+    init(){ 
 }
 }
 
-
-struct STCardView_Previews: PreviewProvider {
- 
-    static var previews: some View {
-        STCardViewModel(wasPushed: .constant(false), card: STCard(message:"preview"))
-        .environmentObject(STCardData())
-    }
-}
